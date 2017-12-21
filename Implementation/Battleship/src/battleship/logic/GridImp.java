88e17f6fb72aeb8bfb1bf4c04817355d49cd9d38
Tang-martin <33925355+Tang-martin@users.noreply.github.com>
2017-12-21 15:08:59 +0100

    added square twodimensional array

--- Implementation/Battleship/src/battleship/logic/GridImp.java
+++ Implementation/Battleship/src/battleship/logic/GridImp.java
@@ -2,6 +2,8 @@ package battleship.logic;
 
 public class GridImp implements Grid {
 	
+	Square[][] squares = new Square[10][10];
+	
 	@Override
 	public void placeShip(Ship ship, int column, int row, boolean isHorizontal) {
 		// TODO Auto-generated method stub
