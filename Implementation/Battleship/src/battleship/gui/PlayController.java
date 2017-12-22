6158ccee1b24c73ce3778a37ff97e44d4c44c901
SsmeaGOLLS <33925161+SsmeaGOLLS@users.noreply.github.com>
2017-12-22 09:23:32 +0100

    Created GUI broken

---
+++ Implementation/Battleship/src/battleship/gui/PlayController.java
@@ -0,0 +1,24 @@
+package battleship.gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
+public class PlayController {
+
+    @FXML
+    private GridPane playerOneTarget;
+
+    @FXML
+    private Label PlayerName;
+
+    @FXML
+    private Label PlayerName2;
+
+    @FXML
+    void clicked(ActionEvent event) {
+
+    }
+
+}
---
+++ Implementation/Battleship/src/battleship/gui/SetFleetController.java
@@ -0,0 +1,73 @@
+package battleship.gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
+public class SetFleetController {
+	private int size = 11;
+
+	@FXML
+    private GridPane fleetBoard;
+	
+    @FXML
+    private ComboBox<?> fleet;
+
+    @FXML
+    private ComboBox<?> orientation;
+
+    @FXML
+    private Button setship;
+
+    @FXML
+    private Label PlayerName;
+
+    @FXML
+    private Button done;
+
+    @FXML
+    void clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void done(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setship(ActionEvent event) {
+    	try {
+    	battleshipController.placeShip(shipClass, column, row, isHorizontal);
+    	Grid grid = battleshipController.getFleetGrid()
+    	for (int column = 1; column<size; column++) {
+    		for ( int row = 1; row<size; row++) {
+    			if (grid.isOccupied(column,row)) {
+    				getNodeFromGridPane(fleetBoard, column, row).setStyle("-fx-background-color: pink");
+    			} else {
+    				getNodeFromGridPane(fleetBoard, column, row).setStyle("-fx-background-color: blue");
+    			}
+    			
+    			
+    			
+    		}
+    	}
+    	}
+    catch(outofbounds) {
+    	
+    }
+    }
+
+    private Node getNodeFromGridPane(GridPane gridPane, int col, int row) {
+	    for (Node node : gridPane.getChildren()) {
+	        if (GridPane.getColumnIndex(node) == col && GridPane.getRowIndex(node) == row) {
+	            return node;
+	        }
+	    }
+	    return null;
+	}
+}
