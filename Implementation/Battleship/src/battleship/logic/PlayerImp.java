5ae3ea0b7416e7a851cab5956b5bf278f9512be9
MikkelTheGreat <33924773+MikkelTheGreat@users.noreply.github.com>
2017-12-22 09:30:09 +0100

    Added opponent
    Added opponent field and added it to the constructor

--- Implementation/Battleship/src/battleship/logic/PlayerImp.java
+++ Implementation/Battleship/src/battleship/logic/PlayerImp.java
@@ -4,12 +4,15 @@ import java.util.HashMap;
 
 public class PlayerImp implements Player {
 
+	private Player opponent;
 	private Grid fleetGrid;
 	private Grid targetGrid;
 	private HashMap Ships;
 	private String name;
+	
 
-	public PlayerImp(Grid fleetGrid, Grid targetGrid, HashMap Ships, String name) {
+	public PlayerImp(Player opponent, Grid fleetGrid, Grid targetGrid, HashMap Ships, String name) {
+		this.opponent = opponent;
 		this.fleetGrid = fleetGrid;
 		this.targetGrid = targetGrid;
 		this.Ships = Ships;
@@ -22,18 +25,12 @@ public class PlayerImp implements Player {
 	}
 
 	@Override
-	public void getShotAt(int column, int row) {
-		// Spoorg Anders
-	}
-
-	@Override
 	public void placeShip(ShipClass shipClass, int column, int row, boolean isHorizontal) {
 		if (Ships.get(shipClass).isPlaced()) {
 			throw new AlreadyPlacedException();
 		} else {
 			fleetGrid.placeShip(Ships.get(shipClass), column, row, isHorizontal);
 		}
-
 	}
 
 	@Override
