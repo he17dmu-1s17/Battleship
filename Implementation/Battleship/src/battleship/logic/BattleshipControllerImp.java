b524f88a762b09dbe3be613ad7d884c3874f0eb7
ShahnazYahyavi <33924824+ShahnazYahyavi@users.noreply.github.com>
2017-12-22 10:12:26 +0100

    implementer initializeGame

--- Implementation/Battleship/src/battleship/logic/BattleshipControllerImp.java
+++ Implementation/Battleship/src/battleship/logic/BattleshipControllerImp.java
@@ -1,18 +1,20 @@
 package battleship.logic;
-
-import java.util.List;
-
 public class BattleshipControllerImp implements BattleshipController {
 	private Player player1;
 	private Player player2;
 	private Player curentPlayer;
 	private boolean isReadyToStart;
 	private boolean gameOver;
-	private boolean gameStarted;//jeg har tilføjet den vi mangler den i complate DCD
+	private boolean gameStarted;// jeg har tilføjet den vi mangler den i complate DCD
+
+	public BattleshipControllerImp(Player player1, Player player2) {
+		this.player1 = player1;
+		this.player2 = player2;
+	}
 
 	@Override
 	public boolean isReadyToStart() {
-		
+
 		return false;
 	}
 
@@ -30,18 +32,20 @@ public class BattleshipControllerImp implements BattleshipController {
 
 	@Override
 	public void startGame() {
-		
+		while (isReadyToStart()) {
+			gameStarted = true;
+		}
 
 	}
 
 	@Override
 	public void placeShip(ShipClass shipClass, int column, int row, boolean isHorizontal) {
-
+		curentPlayer.placeShip(shipClass, column, row, isHorizontal);
 	}
 
 	@Override
-	public boolean isGameOver() { // problem
-		return false;
+	public boolean isGameOver() {
+		return gameOver;
 	}
 
 	@Override
@@ -61,7 +65,8 @@ public class BattleshipControllerImp implements BattleshipController {
 	@Override
 	public void initializeGame() {
 		Player player1 = new PlayerImp();
-		Player Player2 = new PlayerImp();
+		Player player2 = new PlayerImp();
+		BattleshipControllerImp battleshipController = new BattleshipControllerImp(player1, player2);
 	}
 
 	@Override
@@ -70,7 +75,7 @@ public class BattleshipControllerImp implements BattleshipController {
 	}
 
 	private Player endGame() {
-		// vi skal laver allShipsSunk metud i player
+		// vi skal laver allShipsSunk metud i Player
 		Player winner;
 		gameOver = true;
 		if (player1.allShipsSunk()) {
